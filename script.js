a407a91758cff2478a2e32c748152ee1cf77ca39
fariszaqwan1407-byte <fariszaqwan1407@gmail.com>
2026-06-16 19:32:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let stream;
 let imageData;
-let currentFacing="user"; // front default
+let currentFacing = "user";
 
+// START CAMERA
 function startCamera(){
-openCamera(currentFacing);
-
 document.getElementById("welcome").classList.remove("active");
 document.getElementById("cameraScreen").classList.add("active");
+openCamera(currentFacing);
 }
 
-// OPEN CAMERA FUNCTION
+// OPEN CAMERA
 function openCamera(facing){
 
 if(stream){
@@ -26,9 +26,7 @@ document.getElementById("camera").srcObject=s;
 
 // SWITCH CAMERA
 function switchCamera(){
-
 currentFacing = currentFacing === "user" ? "environment" : "user";
-
 openCamera(currentFacing);
 }
 
@@ -43,38 +41,51 @@ canvas.height=video.videoHeight;
 
 let ctx=canvas.getContext("2d");
 
+// draw image
 ctx.drawImage(video,0,0);
 
+// input
 let nama=document.getElementById("nama").value || "";
 let ucapan=document.getElementById("ucapan").value || "";
 
-let h=canvas.height;
+// FRAME AREA
+let h = canvas.height;
 
-// gradient frame
-let gradient=ctx.createLinearGradient(0,h-250,0,h);
+// gradient bottom frame
+let gradient = ctx.createLinearGradient(0, h-300, 0, h);
 gradient.addColorStop(0,"transparent");
 gradient.addColorStop(1,"rgba(0,0,0,0.75)");
 
-ctx.fillStyle=gradient;
-ctx.fillRect(0,h-250,canvas.width,250);
+ctx.fillStyle = gradient;
+ctx.fillRect(0,h-300,canvas.width,300);
 
-// text
+// TEXT STYLE
 ctx.fillStyle="white";
 ctx.textAlign="center";
 
+// TITLE
 ctx.font="bold 42px sans-serif";
-ctx.fillText("FARIS ❤️ SHAFIQAH",canvas.width/2,h-170);
+ctx.fillText("FARIS ❤️ SHAFIQAH",canvas.width/2,h-190);
 
-ctx.font="28px sans-serif";
-ctx.fillText("27 SEPTEMBER 2026",canvas.width/2,h-120);
+// DATE
+ctx.font="26px sans-serif";
+ctx.fillText("27 SEPTEMBER 2026",canvas.width/2,h-140);
 
-ctx.font="bold 32px sans-serif";
-ctx.fillText(nama,canvas.width/2,h-70);
+// NAME
+ctx.font="bold 34px sans-serif";
+ctx.fillText(nama,canvas.width/2,h-90);
 
-ctx.font="24px sans-serif";
-ctx.fillText(ucapan.substring(0,45),canvas.width/2,h-30);
+// UCAPAN
+ctx.font="22px sans-serif";
 
-imageData=canvas.toDataURL("image/jpeg",0.9);
+let shortUcapan = ucapan.length > 50
+? ucapan.substring(0,50)+"..."
+: ucapan;
+
+ctx.fillText(shortUcapan,canvas.width/2,h-45);
+
+// convert
+imageData = canvas.toDataURL("image/jpeg",0.9);
 
 document.getElementById("preview").src=imageData;
 
@@ -82,7 +93,7 @@ document.getElementById("cameraScreen").classList.remove("active");
 document.getElementById("previewScreen").classList.add("active");
 }
 
-// UPLOAD
+// UPLOAD TO DRIVE
 function upload(){
 
 let nama=document.getElementById("nama").value || "guest";
