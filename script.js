df2a2dca83e7a4e0622d3ca943b0a6abe7cbf3b0
fariszaqwan1407-byte <fariszaqwan1407@gmail.com>
2026-06-16 19:09:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 let stream;
 let imageData;
+let currentFacing="user"; // front default
 
-// START CAMERA
 function startCamera(){
+openCamera(currentFacing);
+
 document.getElementById("welcome").classList.remove("active");
 document.getElementById("cameraScreen").classList.add("active");
+}
+
+// OPEN CAMERA FUNCTION
+function openCamera(facing){
+
+if(stream){
+stream.getTracks().forEach(t=>t.stop());
+}
 
 navigator.mediaDevices.getUserMedia({
-video:{facingMode:"user"}
+video:{facingMode:facing}
 }).then(s=>{
 stream=s;
 document.getElementById("camera").srcObject=s;
 });
 }
 
-// CAPTURE + WEDDING FRAME
+// SWITCH CAMERA
+function switchCamera(){
+
+currentFacing = currentFacing === "user" ? "environment" : "user";
+
+openCamera(currentFacing);
+}
+
+// CAPTURE + FRAME
 function capture(){
 
 let video=document.getElementById("camera");
@@ -25,16 +43,14 @@ canvas.height=video.videoHeight;
 
 let ctx=canvas.getContext("2d");
 
-// draw camera
 ctx.drawImage(video,0,0);
 
-// input data
 let nama=document.getElementById("nama").value || "";
 let ucapan=document.getElementById("ucapan").value || "";
 
-// DARK GRADIENT FRAME BOTTOM
 let h=canvas.height;
 
+// gradient frame
 let gradient=ctx.createLinearGradient(0,h-250,0,h);
 gradient.addColorStop(0,"transparent");
 gradient.addColorStop(1,"rgba(0,0,0,0.75)");
@@ -42,27 +58,22 @@ gradient.addColorStop(1,"rgba(0,0,0,0.75)");
 ctx.fillStyle=gradient;
 ctx.fillRect(0,h-250,canvas.width,250);
 
-// TEXT STYLE
+// text
 ctx.fillStyle="white";
 ctx.textAlign="center";
 
-// TITLE
 ctx.font="bold 42px sans-serif";
 ctx.fillText("FARIS ❤️ SHAFIQAH",canvas.width/2,h-170);
 
-// DATE
 ctx.font="28px sans-serif";
 ctx.fillText("27 SEPTEMBER 2026",canvas.width/2,h-120);
 
-// NAME
 ctx.font="bold 32px sans-serif";
 ctx.fillText(nama,canvas.width/2,h-70);
 
-// UCAPAN (short)
 ctx.font="24px sans-serif";
 ctx.fillText(ucapan.substring(0,45),canvas.width/2,h-30);
 
-// convert
 imageData=canvas.toDataURL("image/jpeg",0.9);
 
 document.getElementById("preview").src=imageData;
@@ -71,7 +82,7 @@ document.getElementById("cameraScreen").classList.remove("active");
 document.getElementById("previewScreen").classList.add("active");
 }
 
-// UPLOAD TO GOOGLE DRIVE
+// UPLOAD
 function upload(){
 
 let nama=document.getElementById("nama").value || "guest";
@@ -86,7 +97,7 @@ ucapan:ucapan
 })
 })
 .then(res=>res.json())
-.then(data=>{
+.then(()=>{
 
 document.getElementById("previewScreen").classList.remove("active");
 document.getElementById("done").classList.add("active");
